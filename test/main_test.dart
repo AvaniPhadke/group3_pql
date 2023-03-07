8de04c7263f86958b520ec8a00c5b5f3ec390f03
AvaniPhadke <avani.phadke@yahoo.in>
2023-03-07 23:04:27 +0530

    Testing jenkins - git connection 2

--- test/main_test.dart
+++ test/main_test.dart
@@ -22,7 +22,7 @@ void main() {
 
   Future<void> navigateShowSurroundingsPage(WidgetTester tester) async {
     await tester.tap(
-        find.widgetWithText(RadioListTile<String>, 'Show my surroundings2'),
+        find.widgetWithText(RadioListTile<String>, 'Show my surroundings'),
         warnIfMissed: false);
     await tester.pumpAndSettle();
   }
